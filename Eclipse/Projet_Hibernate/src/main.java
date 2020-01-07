8082d0745c6487b1d8524f7f924184b4c29f29bd
AdrienVerdier <adrien.verdier@gmail.com>
2020-01-07 15:59:26 +0100

    Creation classe vue
    Création des classes de la vue

--- Eclipse/Projet_Hibernate/src/main.java
+++ Eclipse/Projet_Hibernate/src/main.java
@@ -1,9 +1,12 @@
+import view.AppInterface;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		AppInterface Application = new AppInterface();
+		
 	}
 
 }
